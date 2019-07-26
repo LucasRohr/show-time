4f0da1395a0040fca238e5554a40c6360ef832d1
LucasRohr <lucasrc17@live.com>
2019-07-25 21:55:27 -0300

    style(frontend): styling search users modal

--- src/components/search-users-modal/search-users-modal.component.jsx
+++ src/components/search-users-modal/search-users-modal.component.jsx
@@ -3,6 +3,9 @@ import './search-users-modal.component.css';
 import Modal from '@material-ui/core/Modal';
 import Slide from '@material-ui/core/Slide';
 import { SearchUserResult } from '../search-user-result/search-user-result.component';
+import { ReactComponent as AddUser } from '../../assets/icons/user-plus-solid.svg';
+import { ReactComponent as SadFace } from '../../assets/icons/frown-open-regular.svg';
+import { ReactComponent as SearchIcon } from '../../assets/icons/search-solid.svg';
 
 export class SearchUsersModal extends React.PureComponent {
 
@@ -19,11 +22,23 @@ export class SearchUsersModal extends React.PureComponent {
     }
 
     renderNoSearchMadeMessage = () => {
-        
+        return (
+            <div className="no-search-made-container" >
+                <AddUser className="no-search-made-icon" />
+
+                <p className="no-search-made-message" > Não perca tempo, faça novas amizades! </p>
+            </div>
+        );
     }
 
     renderNoSearchResultsMessage = () => {
+        return (
+            <div className="no-search-results-container" >
+                <SadFace className="no-search-results-icon" />
 
+                <p className="no-search-results-message" > Nenhum usuário com esse nome encontrado, tente novamente! </p>
+            </div>
+        );
     }
 
     renderUsersFromSearch = () => {
@@ -42,18 +57,22 @@ export class SearchUsersModal extends React.PureComponent {
 
     render() {
         return (
-            <Modal open={this.props.open} onClose={this.props.onClose} >
+            <Modal className="search-users-modal" open={this.props.open} onClose={this.props.onClose} >
                 <Slide direction="up" in={this.props.open} mountOnEnter unmountOnExit >
                     <div className="modal-search-users-content" >
 
                         <div className="search-users-header" >
                             <p className="search-users-modal-title" > Procure por pessoas e faça amizades! </p>
 
-                            <input type="text"
-                                className="user-search-input"
-                                onChange={ (text) => this.setSearchText(text) }
-                                placeholder="Digite o nome de alguém"
-                            />
+                            <div className="search-users-input-container" >
+                                <input type="text"
+                                    className="user-search-input"
+                                    onChange={ (text) => this.setSearchText(text) }
+                                    placeholder="Digite o nome de alguém"
+                                />
+
+                                <SearchIcon className="user-search-icon" />
+                            </div>
                         </div>
 
                         {
