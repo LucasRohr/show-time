ed3bb2556b85e99d51af804cfa3b5943f6794ce7
LucasRohr <lucasrc17@live.com>
2019-08-01 16:38:23 -0300

    feat(frontend): adding structure to open recommendation in recommendation modal

--- src/components/recommendation-modal/recommendation-history-modal.component.jsx
+++ src/components/recommendation-modal/recommendation-history-modal.component.jsx
@@ -3,6 +3,8 @@ import './recommendation-history-modal.component.css';
 import Modal from '@material-ui/core/Modal';
 import Slide from '@material-ui/core/Slide';
 import { ButtonCommon } from '../button-common/button-common.component';
+import { Recommendation } from '../../models/recommendation';
+import { RecommendationItem } from './recommendation-item/recommendation-item.component';
 
 export class RecommendationHistoryModal extends React.PureComponent {
 
@@ -10,8 +12,21 @@ export class RecommendationHistoryModal extends React.PureComponent {
         super(props);
         this.state = {
             isCreateRecModalOpen: false,
-            sentRecommendations: [],
-            receivedRecommendations: []
+            isSentRecommendationsListSelected: true,
+            sentRecommendations: [
+                new Recommendation(
+                    1, 1, "jao", "", 2, "kleberzinho", "", new Date(), "ve TEOTFW, mui bueno", true,
+                    "The End of The Fucking World", true, "mt bom mano, tu olha rapidinho, eu adorei"
+                )
+            ],
+            receivedRecommendations: [
+                new Recommendation(
+                    2, 2, "kleberzinho", "", 1, "jao", "", new Date(), "ve GoT de uma vez cara, até ja acabou mds", true,
+                    "Game of Thrones", false, "melhot serie ja feita, cheio de personagens daora, pena que o final foi bem +-"
+                )
+            ],
+            isRecommendationModalOpen: false,
+            selectedRecommendation: null
         }
     }
 
@@ -19,6 +34,50 @@ export class RecommendationHistoryModal extends React.PureComponent {
         this.setState( prevState => ({
             isCreateRecModalOpen: !prevState.isCreateRecModalOpen
         }));
+    }
+
+    renderSentRecommendationsList = () => {
+        this.state.sentRecommendations.map(
+            (sentRecommendationItem, key) => {
+                <RecommendationItem
+                    key={key}
+                    userAvatar={sentRecommendationItem.recipientAvatar}
+                    username={sentRecommendationItem.recipientUsername}
+                    openRecommendation={() => { this.openRecommendationModal(receivedRecommendationItem) }}
+                />
+            }
+        )
+    }
+
+    renderReceivedRecommendationsList = () => {
+        this.state.receivedRecommendations.map(
+            (receivedRecommendationItem, key) => {
+                <RecommendationItem
+                    key={key}
+                    userAvatar={receivedRecommendationItem.recipientAvatar}
+                    username={receivedRecommendationItem.recipientUsername}
+                    openRecommendation={() => { this.openRecommendationModal(receivedRecommendationItem) }}
+                />
+            }
+        )
+    }
+
+    handleIsRecommendationOpen = () => {
+        this.setState( prevState => ({
+            isRecommendationModalOpen: !prevState.isRecommendationModalOpen
+        }));
+    }
+
+    openRecommendationModal = (recommendation) => {
+        this.setState({
+            selectedRecommendation: new Recommendation(
+                recommendation.id, recommendation.authorId, recommendation.authorUsername,
+                recommendation.authorAvatar, recommendation.recipientId, recommendation.recipientUsername,
+                recommendation.recipientAvatar, recommendation.datetime, recommendation.title,
+                recommendation.isShow, recommendation.showName, recommendation.hasInNetflix, recommendation.review
+            )
+        })
+        this.handleIsRecommendationOpen();
     }
 
     render() {
@@ -51,9 +110,24 @@ export class RecommendationHistoryModal extends React.PureComponent {
                         </button>
                     </div>
 
-                    
+                    {
+                        this.state.isSentRecommendationsListSelected ?
+                            this.renderSentRecommendationsList()
+                        :
+                            this.renderReceivedRecommendationsList()
+                    }
 
-
+                    <Recommendation
+                        open={this.state.isRecommendationModalOpen}
+                        onClose={this.handleIsRecommendationOpen}
+                        senderAvatar={this.state.selectedRecommendation.authorAvatar}
+                        senderUsername={this.state.selectedRecommendation.authorUsername}
+                        title={this.state.selectedRecommendation.title}
+                        isShow={this.state.selectedRecommendation.isShow}
+                        showName={this.state.selectedRecommendation.showName}
+                        hasInNetflix={this.state.selectedRecommendation.hasInNetflix}
+                        recommendationReview={this.state.selectedRecommendation.review}
+                    />
 
                 </Slide>
             </Modal>
